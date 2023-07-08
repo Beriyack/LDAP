bb7becb3fd98f1bec58abf6d54a1ed101a95d04f
Beriyack <33500849+Beriyack@users.noreply.github.com>
2023-07-08 19:42:26 +0000

    Commit de test

--- example/add_ben.php
+++ example/add_ben.php
@@ -3,4 +3,7 @@
     require_once './shared/header.php';
 ?>
 <h3><?= $title ?></h3>
+<form action="./Controller/BeneficiaireController.php" method="post">
+    
+</form>
 <?php require_once './shared/footer.php'; ?>
